a3fe064194d16983446766b3a3fce188a46f6604
lwouis <lwouis@gmail.com>
2016-05-21 17:18:37 +0200

    Version 1.0

---
+++ LaBanquePostaleBatchPdfDownloader.user.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name         LaBanquePostaleBatchPdfDownloader
+// @namespace    com.lwouis
+// @version      1.0
+// @description  Once manually logged on LBP's website, the script will show a button to download all PDFs related to operations on your account. The button will be located in "Consulter vos e-relevés" > "Historique de mes relevés". The script does what's already possible through the UI, but downloads everything with one click instead of manual labor.
+// @author       Louis Pontoise
+// @match        https://voscomptesenligne.labanquepostale.fr/voscomptes/canalXHTML/relevePdf/relevePdf_historique/reinitialiser-historiqueRelevesPDF.ea
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    var input = addBatchDownloadButton();
+    input.addEventListener('click', function (e) {
+        e.preventDefault();
+        var select = document.getElementById("numeroCompteRecherche");
+        var accountId = select.options[select.selectedIndex].value;
+        var currentYear = new Date().getFullYear();
+        downloadAllPdfForThatYear(accountId, currentYear);
+    }, false);
+
+    function addBatchDownloadButton() {
+        var ul = document.getElementsByClassName("actions")[0];
+        var li = document.createElement("li");
+        li.classList.add('btn');
+        li.classList.add('med');
+        var input = document.createElement("input");
+        input.classList.add('btn');
+        input.type = "submit";
+        input.value = "Batch download";
+        li.appendChild(input);
+        ul.insertBefore(li, ul.childNodes[ul.childNodes.length]);
+        return input;
+    }
+
+    function downloadAllPdfForThatYear(accountId, currentYear) {
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: "https://voscomptesenligne.labanquepostale.fr/voscomptes/canalXHTML/relevePdf/relevePdf_historique/form-historiqueRelevesPDF.ea?formulaire.numeroCompteRecherche=" + accountId + "&formulaire.anneeRecherche=" + currentYear + "&formulaire.moisRecherche=",
+            onload: function (response) {
+                var downloadLinksRegex = /"(preparerTelechargementPDF[^"]*)"/g;
+                var matches = [];
+                var match;
+                while (match = downloadLinksRegex.exec(response.responseText)) {
+                    matches.push(match[1]);
+                }
+                if (matches.length > 0) {
+                    downloadNextPdf(accountId, matches, 0, currentYear);
+                }
+            }
+        });
+    }
+
+    function downloadNextPdf(accountId, matches, i, currentYear) {
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: "https://voscomptesenligne.labanquepostale.fr/voscomptes/canalXHTML/relevePdf/relevePdf_historique/" + matches[i],
+            onload: function () {
+                var form = document.createElement("form");
+                form.action = "https://voscomptesenligne.labanquepostale.fr/voscomptes/canalXHTML/relevePdf/relevePdf_historique/telechargerPDF-historiqueRelevesPDF.ea";
+                form.target = "_blank";
+                form.submit();
+                window.setTimeout(function () {
+                    if (i + 1 < matches.length) {
+                        downloadNextPdf(accountId, matches, i + 1, currentYear);
+                    }
+                    else {
+                        downloadAllPdfForThatYear(accountId, currentYear - 1);
+                    }
+                }, 2000);
+            }
+        });
+    }
+})();
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# LaBanquePostaleBatchPdfDownloader
+# Purpose
+
+This is a userscript which allows you to batch download all your PDF bank statements from LaBanquePostale website with the click of a button.
+
+# How to install
+
+* If you don't have it already, install [Tampermonkey](https://tampermonkey.net/) (Chrome) or [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) (Firefox)
+* Click on this [link to the raw version of the .user.js file](https://github.com/lwouis/LaBanquePostaleBatchPdfDownloader/raw/master/LaBanquePostaleBatchPdfDownloader.user.js)
+* Tampermonkey should recognize the userscript and install it
+
+# How to use
+
+* Log into your LaBanquePostale account
+* Go into "Consulter vos e-relevés" > "Historique de mes relevés"
+* Select the account you are interested in with the dropdown menu
+* Click on the "Batch download" button
+* The PDFs will download one after the other
